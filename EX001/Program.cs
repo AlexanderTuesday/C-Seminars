e60b7b3f9a0ffe2942ea9dbd8f24890f58c9b258
AVtor <vtorik1@mail.ru>
2023-03-01 17:36:06 +0300

    added 001

---
+++ EX001/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Введите целое число: ");
+
+string userInputStr = Console.ReadLine() ?? "";
+int userNumber = int.Parse(userInputStr);
+
+int result = userNumber * userNumber;
+Console.WriteLine($"{userNumber} -> {result}");
