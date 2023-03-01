8834ac2350f884d53cbdce0194f5b53bbd0ebf0b
AVtor <vtorik1@mail.ru>
2023-03-01 17:40:48 +0300

    added 002

---
+++ EX002/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите целое число: ");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите целое число: ");
+int numB = Convert.ToInt32(Console.ReadLine());
+
+if (numA == numB * numB)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
